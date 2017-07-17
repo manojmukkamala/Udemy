8268196303df86670676595f9a5f5ff3c8b2983b
Manoj Mukkamala <manojmukkamala11@gmail.com>
2017-07-17 16:54:22 +0530

    Create my_multiple_linear_regression.R

---
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/my_multiple_linear_regression.R
@@ -0,0 +1,18 @@
+# Importing the dataset
+dataset = read.csv('50_startups.csv')
+
+# Handling categorical data
+dataset$State = factor(dataset$State,
+                                 levels = c('New York', 'California', 'Florida'),
+                                 labels = c(1, 2, 3))
+
+# Splitting the training and test sets
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == T)
+test_set = subset(dataset, split == F)
+
+# Fitting Multiple Linear Regression to the training set
+regressor = lm(formula = Profit ~ ., data = training_set)
+summary(regressor)
